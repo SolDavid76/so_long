1921b58695903a0add8fcea91ca97ccae381d777
SolDavid76 <david.janusz75@gmail.com>
2023-01-22 15:39:29 +0100

    gnl

---
+++ gnl.c
@@ -0,0 +1,38 @@
+#include "so_long.h"
+
+static char	*ft_readstash(int fd, char *str)
+{
+	int		n;
+	char	*tmp;
+
+	n = 1;
+	tmp = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!tmp)
+		return (NULL);
+	while (!ft_strchr(str, '\n') && n != 0)
+	{
+		n = read(fd, tmp, BUFFER_SIZE);
+		if (n == -1)
+			return (free(tmp), NULL);
+		if (n == 0)
+			return (free(tmp), str);
+		tmp[n] = '\0';
+		str = ft_strjoin(str, tmp);
+	}
+	return (free(tmp), str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = ft_readstash(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = ft_get_line(stash);
+	stash = ft_clean(stash);
+	return (free(stash), NULL);
+}
---
+++ gnl_aux.c
@@ -0,0 +1,106 @@
+#include "so_long.h"
+
+static int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+static int	ft_strchr(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*res;
+	int		i;
+	int		j;
+
+	if (!s2)
+		return (NULL);
+	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	j = 0;
+	if (s1)
+	{
+		while (s1[i])
+			res[j++] = s1[i++];
+	}
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i++];
+	res[j] = '\0';
+	if (s1)
+		free(s1);
+	return (res);
+}
+
+char	*ft_get_line(char *str)
+{
+	int		i;
+	char	*res;
+
+	i = 0;
+	if (str[i] == '\0')
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	res = malloc(sizeof(char) * (i + 2));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		res[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		res[i] = str[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char	*ft_clean(char *str)
+{
+	char	*res;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\0')
+		return (free(str), NULL);
+	res = malloc(sizeof(char) * (ft_strlen(str) - i + 2));
+	if (!res)
+		return (NULL);
+	i++;
+	j = 0;
+	while (str[i])
+		res[j++] = str[i++];
+	res[j] = '\0';
+	return (free(str), res);
+}
